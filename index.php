ce0500b375df11683e9d0d92452fc88739baffc7
Jaque-81 <jaqueline.lima.jpl@gmail.com>
2022-04-27 09:00:08 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    $paginas = ['home', 'sobre', 'contato'=>'<input type="text"/>'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Site PHP</title>
+</head>
+<body>
+    <?php
+        echo $paginas ['contato'];
+    ?>
+</body>
+</html>
