bda8acdf152c8b0374c8a809521d25f7b266de51
Jaque-81 <jaqueline.lima.jpl@gmail.com>
2022-04-27 16:26:36 -0300

    master

--- index.php
+++ index.php
@@ -1,19 +1,36 @@
 <?php
-    $paginas = ['home', 'sobre', 'contato'=>'<input type="text"/>'];
-
+    $paginas = ['home'=> 'Minha página home aqui!', 'sobre'=> 'Estpu na página sobre', 'contato'=>''];
+    
+    $paginas ['contato'] = '<form><input type="text" placeholder="Digite seu nome: ... "/></form>' //html
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title> Site PHP</title>
+    <style type="text/css">
+        *{
+            margin: 0;
+            padding: 0;
+           box-sizing: border-box;
+            text-align: center;
+            
+        }
+        header{
+                
+                background-color: #069;
+                padding: 8px 10px;
+                text-align: center;
+        }
+    </style>
 </head>
 <body>
     <?php
-        echo $paginas ['contato'];
+        foreach ($paginas as $key => $value){
+            echo '<a href="?page='.$key.'">'.$key.'</a>';
+
+        }
     ?>
 </body>
 </html>
